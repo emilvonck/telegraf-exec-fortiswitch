1d5f629a685a21917980eadddf6a13b12a6f9c08
Emil Vonck <emil.vonck@gmail.com>
2022-10-25 10:58:14 +0200

    added logger

--- fortiswitch/core/request.py
+++ fortiswitch/core/request.py
@@ -1,7 +1,13 @@
 """Functions for requests."""
+import sys
+
+import requests
 from requests.sessions import Session
 
 from fortiswitch.core.session import logout
+from fortiswitch.logging import LogHandler
+
+logging = LogHandler(__name__)
 
 HEADERS = {"Accept": "application/json"}
 
@@ -16,7 +22,19 @@ def req(session: Session, url: str):
     Returns:
         _type_: requests.Response()
     """
-    response = session.get(url=url, headers=HEADERS, verify=session.ignore_ssl)
-    logout(session)
+    try:
+        response = session.get(url=url, headers=HEADERS, verify=session.ignore_ssl)
+        log_dict = {
+            "level": 20,
+            "message_type": "HTTPSuccess",
+            "message": url,
+            "http_response_code": response.status_code,
+        }
+        logging.format_logs(**log_dict)
+        return response
+    except requests.exceptions.HTTPError as err:
+        logging.format_logs(level=40, message_type="HTTPError", message=err)
+        sys.exit(1)
 
-    return response
+    finally:
+        logout(session)
